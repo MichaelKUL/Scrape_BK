3e17c2ec4a135af8bbd9ca7f89cedf7d2c000307
VetMichael <vte.michael@gmail.com>
2022-12-01 12:50:21 +0100

    final scraper with yelp

--- Scraping_BK_Func.py
+++ Scraping_BK_Func.py
@@ -9,6 +9,7 @@ import pandas as pd
 import os.path
 from datetime import date, datetime
 import time
+import json
 
 
 def Initiate_BK_DF():
@@ -19,14 +20,19 @@ def Initiate_BK_DF():
 
 
 
-def Get_BK_Data(pd_df):
+def Get_BK_Data_TA(pd_df):
     
     pages = range(0, 255, 15)
-    i = 0
+
+    try:
+        i = max(pd_df.index)
+    except ValueError as error:
+        i = 0
 
     for page in pages:
 
         r = requests.get('https://www.tripadvisor.com/Restaurant_Review-g186338-d804929-Reviews-or' + str(page) + '-Burger_King-London_England.html', headers={"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"})
+        
         html_data = r.text
         
         soup = BeautifulSoup(html_data, "lxml")
@@ -47,12 +53,50 @@ def Get_BK_Data(pd_df):
     
     return pd_df
 
+def Get_BK_Data_YE(pd_df):
+    
+    pages = range(0, 255, 15)
+
+    try:
+        i = max(pd_df.index)
+    except ValueError as error:
+        i = 0
+
+    for page in pages:
+
+        r = requests.get('https://www.yelp.com/biz/burger-king-london-28', headers={"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"})
+        
+        html_data = r.text
+        
+        soup = BeautifulSoup(html_data, "lxml")
+        #soup2 = soup.find_all("script", {"type":"application/ld+json"})
+
+        reviews = soup.find_all("script", {"type":"application/ld+json"})
+        js_reviews = json.loads(reviews[2].string)
+        js_reviews = js_reviews.get("review")
+        #<script data-rh="true" type="application/ld+json"
+        
+        for review in js_reviews:
+
+            review_entry = review["description"]
+
+            review_date = review["datePublished"]
+            review_date = datetime.strptime(review_date, '%Y-%m-%d')
+            
+            row = pd.DataFrame({'Date':[review_date], 'Review':[review_entry], 'Platform':["Yelp"]}, index=[i])
+            pd_df = pd.concat([pd_df, row])
+
+            i += 1
+    
+    return pd_df
+
 
 
 def Save_BK_Data(pd_df):
-    
     print("Saving data to df_Reviews_BK")
     pd_df.to_pickle('df_Reviews_BK')
+
+def Write_BK_Data(pd_df):
     pd_df.to_csv('df_Reviews_BK.csv')
     with pd.ExcelWriter('df_Review_BK.xlsx') as writer:
         pd_df.to_excel(writer)
@@ -61,7 +105,6 @@ def Save_BK_Data(pd_df):
 
 def Daily_Func():
     if os.path.exists('df_Reviews_BK'):
-        #df_BK = Get_BK_Data(pd.read_pickle('df_Reviews_BK'))
 
         print("Dataframe found. Deleting old Dataframes.")
         try:
@@ -71,16 +114,17 @@ def Daily_Func():
             os.rmdir('__pycache__')
         except OSError as error:
             print(error)
-        
-
 
         time.sleep(5)
 
         print('creating a new Dataframe')
         Initiate_BK_DF()
 
-        df_BK = Get_BK_Data(pd.read_pickle('df_Reviews_BK'))
+        df_BK = Get_BK_Data_TA(pd.read_pickle('df_Reviews_BK'))
+        df_BK = Get_BK_Data_YE(df_BK)
+
         Save_BK_Data(df_BK)
+        Write_BK_Data(pd.read_pickle('df_Reviews_BK'))
 
 
     else:
